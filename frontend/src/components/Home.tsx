06e0ba34913b395b478a597a4cbd4e6853df7a5a
kbb-kebab <84271071+kbb-kebab@users.noreply.github.com>
2023-11-16 09:44:02 +0800

    Added the display highest donors feature

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { WalletSDK } from "@roninnetwork/wallet-sdk";
 import axios from "axios";
-// import { Web3Provider } from "@ethersproject/providers";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { CardMedia, Typography } from "@mui/material";
-// import { ExternallyOwnedAccount } from "@ethersproject/abstract-signer";
 
 export default function Home() {
-  // const [userWalletAddress, setUserWalletAddress] = useState("");
   const [userWalletAddress, setUserWalletAddress] = useState(
     sessionStorage.getItem("userWalletAddress") || undefined
   );
   const [totalDonationValue, setTotalDonationValue] = useState(0);
+  const [top5Donors, setTop5Donors] = useState({});
+
+  const donationAddress = "0x8f11877d6181484568b93b30039f5418f787c61c";
 
   const config = {
     method: "get",
     maxBodyLength: Infinity,
     // url: "https://saigon-testnet.roninchain.com/rpc/accounts/0x60f642408a5e661da0489081f2951d046769ca6f", // Replac
     // url: "https://api-gateway.skymavis.com/skynet-test/ronin/accounts/0x60f642408a5e661da0489081f2951d046769ca6f", // Test Net
-    url: "https://api-gateway.skymavis.com/skynet/ronin/accounts/0x60f642408a5e661da0489081f2951d046769ca6f",
+    url: `https://api-gateway.skymavis.com/skynet/ronin/accounts/${donationAddress}`,
     headers: {
       Accept: "application/json",
       "X-API-KEY": process.env.REACT_APP_X_API_KEY,
@@ -32,14 +32,12 @@ export default function Home() {
     const calcTotalDonationValue = async () => {
       return axios(config)
         .then((response: { data: any }) => {
-          // console.log(response.data.result);
-          // console.log(JSON.stringify(response.data.result.ronNetWorth));
-          setTotalDonationValue(response.data.result.ronNetWorth);
-          return response.data.balance; // return the balance value
+          setTotalDonationValue(response.data.result.balance);
+          return response.data.balance;
         })
         .catch((error: any) => {
           console.log(error);
-          throw error; // re-throw the error to be handled by the caller
+          throw error;
         });
     };
 
@@ -52,14 +50,59 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const checkRoninInstalled = () => {
-    if ("ronin" in window) {
-      return true;
-    }
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `https://api-gateway.skymavis.com/explorer/txs/${donationAddress}`,
+        headers: {
+          Accept: "application/json",
+          "X-API-Key": process.env.REACT_APP_X_API_KEY,
+        },
+      };
 
-    window.open("https://wallet.roninchain.com", "_blank");
-    return false;
-  };
+      axios(config)
+        .then((response) => {
+          const data = response.data.results;
+          const dictionary: { [key: string]: number } = {};
+
+          for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            if (item.to === `${donationAddress}` && item.status === 1) {
+              const from = item.from;
+              const value = Number(item.value);
+
+              if (dictionary[from]) {
+                dictionary[from] += value;
+              } else {
+                dictionary[from] = value;
+              }
+            }
+          }
+
+          // Sort the dictionary by value in descending order
+          const sortedArray = Object.entries(dictionary).sort(
+            ([, a], [, b]) => b - a
+          );
+
+          // Limit to the top 5 entries
+          const top5Array = sortedArray.slice(0, 5);
+
+          // Convert the array back to a dictionary
+          const top5Dictionary = Object.fromEntries(top5Array);
+          setTop5Donors(top5Dictionary);
+
+          console.log(top5Dictionary);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 3000); // Runs every 5 seconds
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     const accountChangeListener = (
@@ -84,6 +127,15 @@ export default function Home() {
     };
   }, []);
 
+  const checkRoninInstalled = () => {
+    if ("ronin" in window) {
+      return true;
+    }
+
+    window.open("https://wallet.roninchain.com", "_blank");
+    return false;
+  };
+
   const connectRoninWallet = async () => {
     const sdk = new WalletSDK();
     await sdk.connectInjected();
@@ -107,51 +159,28 @@ export default function Home() {
   const handleConnectWallet = async () => {
     if (userWalletAddress) {
       console.log("connected user wallet address", userWalletAddress);
-      console.log("Wallet is already connected"); // make this into a modal
     }
     // else if (userWalletAddress === undefined) {
     //   console.log("Log in to your Ronin Account through the extension"); // make this into a modal
     // }
     else {
       connectRoninWallet();
-      // const sdk = new WalletSDK();
-      // await sdk.connectInjected();
-
-      // const isInstalled = checkRoninInstalled();
-      // if (isInstalled === false) {
-      //   console.log("Open installation window");
-      // }
-
-      // const accounts = await sdk.requestAccounts();
-      // if (accounts) {
-      //   console.log(`Wallet Connected! Current address: ${accounts[0]}`);
-      //   setUserWalletAddress(accounts[0]);
-      //   // Also save the new userWalletAddress in sessionStorage
-      //   sessionStorage.setItem("userWalletAddress", accounts[0]);
-      // } else {
-      //   console.log("Failed to connect to the wallet");
-      // }
     }
   };
 
   const handleDonate = async () => {
-    console.log("userWalletAddress donate clicked:", userWalletAddress);
-    //^remove this after
     if (userWalletAddress === undefined) {
       console.log("Log in to your Ronin Account through the extension");
       connectRoninWallet();
-      // I want it to automatically open the ronin wallet window here
     } else {
       try {
-        const valueInWei = (0.00001 * 10 ** 18).toString(16);
         await window.ronin!.provider.request({
           method: "eth_sendTransaction",
           params: [
             {
-              to: "0x8f11877d6181484568b93b30039f5418f787c61c",
+              to: donationAddress,
               from: userWalletAddress,
-              // value: "0xDE0B6B3A7640000", // 1000000000000000000, 1 RON
-              value: "0x" + valueInWei, // 0.00001 RON
+              value: "0xDE0B6B3A7640000", // 1000000000000000000, 1 RON
               gas: "0x5208",
               data: "",
             },
@@ -183,8 +212,15 @@ export default function Home() {
           image="../assets/axie_1.png" // path to your image
         />
         <Typography variant="h5" gutterBottom>
-          Total Donated Value: {totalDonationValue.toFixed(5)} RON
+          Total Donated Value: {totalDonationValue} WEI
         </Typography>
+
+        {Object.entries(top5Donors).map(([key, value], index) => (
+          <Typography key={index} variant="body1">
+            {key}: {value as number}
+          </Typography>
+        ))}
+
         <Button
           variant="contained"
           color="primary"
